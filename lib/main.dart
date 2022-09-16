7dc0a35b96e8a45ae7121c4dc376ab8e3fb1051d
DekiFirmansyah <dekifirmansyah99@gmail.com>
2022-09-16 15:32:42 +0700

    Layout flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'component/berita.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -39,20 +41,27 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text("MyApp"),
           backgroundColor: Colors.amber,
         ),
-        body: Column(
-          //child: Row(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
+        body: ListView(
           children: [
             Container(
               padding: const EdgeInsets.all(20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: const [
-                  Text('BERITA TERBARU',
-                      style: TextStyle(fontSize: 15, fontFamily: 'open sans')),
-                  Text('PERTANDINGAN HARI INI',
-                      style: TextStyle(fontSize: 15, fontFamily: 'open sans')),
+                  Text(
+                    'BERITA TERBARU',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'open sans',
+                    ),
+                  ),
+                  Text(
+                    'PERTANDINGAN HARI INI',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'open sans',
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -62,7 +71,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
                   Image.network(
                     'https://i.pinimg.com/564x/30/df/01/30df01af8b1b028d163d42884327df91.jpg',
                     fit: BoxFit.fitWidth,
@@ -72,59 +80,33 @@ class _MyHomePageState extends State<MyHomePage> {
                     color: Colors.white,
                     alignment: Alignment.center,
                     padding: const EdgeInsets.all(12.0),
-                    child: const Text('Timnas Garuda TC Di Eropa',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'open sans')),
+                    child: const Text(
+                      'Timnas Garuda TC Di Eropa',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'open sans',
+                      ),
+                    ),
                   ),
                   Container(
-                      padding: const EdgeInsets.all(10.0),
-                      alignment: Alignment.centerLeft,
-                      child: const Text('Training Camp',
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontFamily: 'open sans'))),
+                    padding: const EdgeInsets.all(10.0),
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      'Training Camp',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontFamily: 'open sans',
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Card(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: Image.network(
-                          'https://i.pinimg.com/564x/41/9b/72/419b729c6b3e15a52087a447077c2b53.jpg',
-                          width: 100,
-                        ),
-                      ),
-                      // ignore: prefer_const_constructors
-                      Expanded(
-                        flex: 4,
-                        child: const Text(
-                            'Timnas Indonesia Raih AFF Suzuki Cup 2020 Fair Play Team Award',
-                            style: TextStyle(
-                                fontSize: 14, fontFamily: 'open sans'),
-                            textAlign: TextAlign.center),
-                      ),
-                    ],
-                  ),
-                  Container(
-                      color: Colors.blue,
-                      padding: const EdgeInsets.all(10.0),
-                      alignment: Alignment.centerLeft,
-                      child: const Text('AFF Suzuki Cup 2020',
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              fontFamily: 'open sans'))),
-                ],
-              ),
-            ),
+            const berita(),
+            const berita(),
+            const berita(),
           ],
         ),
       ),
